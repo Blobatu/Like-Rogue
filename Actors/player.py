9ecec364e6d6d2447b21522e05d3f44b22273e11
Luc Desforges <desforgesluc00@gmail.com>
2026-04-14 11:25:54 -0400

    cleanup to be pretty

--- Actors/player.py
+++ Actors/player.py
@@ -6,11 +6,18 @@ Description : TODO
 import Actors.actor as a
 class NPC(a.Actor):
 
-    def __init__(self, level, name, health = 1, damage = 1, damage_done = 0):
+    def __init__(self, 
+                 level: int,
+                 damage_done: int,
+                 damage_received: int,
+                 name: str, 
+                 position: tuple[int, int], 
+                 health: int = 1, 
+                 damage: int = 1):
         """
         description: Constructeur
         """
-        super().__init__(name, health, damage)
+        super().__init__(name, position, health, damage)
         self.level = level
         self.damage_done = damage_done
-        self.damage_received = damage_done
+        self.damage_received = damage_received
