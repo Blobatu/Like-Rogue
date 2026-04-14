77258d7f65890fde8092c4887a5cb75c0b077262
James-Bergeron <202533444@cegepsherbrooke.qc.ca>
2026-04-14 11:54:44 -0400

    part1.1

--- UI/UI.py
+++ UI/UI.py
@@ -3,24 +3,33 @@ import customtkinter
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("500x350")
+app = customtkinter.CTk()
+app.title("Like-Rogue")
+app.geometry("800x600")
+
+label = customtkinter.CTkLabel(master=app, text="Like-Rogue", font=("Pixel", 24, "bold"))
+label.pack(pady=12, padx=10)
+
+
 
 def play():
     print("Play button clicked")
 
 
 
-    frame = customtkinter.CTkFrame(master=root)
+    frame = customtkinter.CTkFrame(master=app)
     frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-    label = customtkinter.CTkLabel(master=frame, text="Like-Rogue", text_font=("Pixel", 24))
-    label.pack(pady=12, padx=10)
+    play_button = customtkinter.CTkButton(master=frame, text="Play", command=play)
+    play_button.pack(pady=12, padx=10)
 
-    button = customtkinter.CTkButton(master=frame, text="Play", command=play)
-    button.pack(pady=12, padx=10)
+def open_settings():    
+    print("Settings button clicked")
 
-    button = customtkinter.CTkButton(master=frame, text="Settings")
-    button.pack(pady=12, padx=10)
+    settings_button = customtkinter.CTkButton(app, text="Settings", command=open_settings)
+    settings_button.pack(pady=12, padx=10)
 
-    root.mainloop()
+    exit_button = customtkinter.CTkButton(app, text="Exit", command=app.quit)
+    exit_button.pack(pady=12, padx=10)
+
+    app.mainloop()
