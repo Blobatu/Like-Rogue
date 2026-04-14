d0b2a1bab7a56595e453cd286d36986db29decaf
Luc Desforges <desforgesluc00@gmail.com>
2026-04-14 10:57:55 -0400

    Update npc_interactions.py

--- Interactions/npc_interactions.py
+++ Interactions/npc_interactions.py
@@ -1,6 +1,6 @@
 import Actors.npc as npc
 
-converter_eps_tool = npc.NPC(name = "Goblin", 
+gob = npc.NPC(name = "Goblin", 
                              health = 1, 
                              damage = 1, 
                              zone = 1)
