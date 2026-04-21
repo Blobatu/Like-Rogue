b93362bae38f05deb6e99d813aa62eb70f5fc6f6
Léonard Lefebvre <leonardlefebvre5@gmail.com>
2026-04-21 12:37:38 -0400

    why

--- Interactions/player_interactions.py
+++ Interactions/player_interactions.py
@@ -4,9 +4,9 @@ date:14 avril
 description:to do
 '''
 import Actors.player as p
-walls = '##'
-player = '@ '
-air = '. '
+walls = "##"
+player = "@ "
+air = ". "
 #player_position = player.position
 mobs =("player","other_monsters")
 
@@ -22,46 +22,69 @@ test_print = ["####################",
 global player_actor
 player_actor = p.get_player()
 global player_position
-player_position = (10, 3) 
+player_position = (4, 3)  # col 4, row 3
 player_actor.position = player_position
 
-
+def is_wall(col, row):
+    if row < 0 or row >= len(test_print) or col < 0 or col >= 10:
+        return True
+    return test_print[row][col*2:col*2+2] == walls
 
 def move_up():
     global test_print, player_position
-    if (player_actor.position[0],player_actor.position[1]+1) == walls:
+    col, row = player_actor.position
+    new_row = row - 1
+    if is_wall(col, new_row):
         print("You can't move there")
-
     else:
-        chars = test_print
-
-        print(player_actor.position)
-        chars = list(chars[player_actor.position[1]-1])
-        chars[player_actor.position[0]] = player
-        player_actor.position=(player_actor.position[0],player_actor.position[1]-1)
+        # clear old position
+        test_print[row] = test_print[row][:col*2] + air + test_print[row][col*2+2:]
+        # set new position
+        test_print[new_row] = test_print[new_row][:col*2] + player + test_print[new_row][col*2+2:]
+        player_actor.position = (col, new_row)
         player_position = player_actor.position
-        test_print[player_actor.position[1]] = "".join(chars)
-        print(player_actor.position)
+
 def move_down():
     global test_print, player_position
-    if (player_actor.position[0],player_actor.position[1]-1) == walls:
+    col, row = player_actor.position
+    new_row = row + 1
+    if is_wall(col, new_row):
         print("You can't move there")
     else:
-        player_actor.position=(player_actor.position[0],player_actor.position[1]-1)
+        # clear old position
+        test_print[row] = test_print[row][:col*2] + air + test_print[row][col*2+2:]
+        # set new position
+        test_print[new_row] = test_print[new_row][:col*2] + player + test_print[new_row][col*2+2:]
+        player_actor.position = (col, new_row)
+        player_position = player_actor.position
+
 def move_left():
     global test_print, player_position
-    if (player_actor.position[0]-1,player_actor.position[1]) == walls:
+    col, row = player_actor.position
+    new_col = col - 1
+    if is_wall(new_col, row):
         print("You can't move there")
     else:
-        player_actor.position=(player_actor.position[0]-1,player_actor.position[1])
+        # clear old position
+        test_print[row] = test_print[row][:col*2] + air + test_print[row][col*2+2:]
+        # set new position
+        test_print[row] = test_print[row][:new_col*2] + player + test_print[row][new_col*2+2:]
+        player_actor.position = (new_col, row)
+        player_position = player_actor.position
+
 def move_right():
     global test_print, player_position
-    new_row = player_actor.position[0]
-    new_col = player_actor.position[1] + 1
-    if player_actor.position == (player_actor.position[0]+1,player_actor.position[1])== walls:
+    col, row = player_actor.position
+    new_col = col + 1
+    if is_wall(new_col, row):
         print("You can't move there")
     else:
-        player_actor.position=(player_actor.position[0]+1,player_actor.position[1])
+        # clear old position
+        test_print[row] = test_print[row][:col*2] + air + test_print[row][col*2+2:]
+        # set new position
+        test_print[row] = test_print[row][:new_col*2] + player + test_print[row][new_col*2+2:]
+        player_actor.position = (new_col, row)
+        player_position = player_actor.position
 
 
 
@@ -122,7 +145,7 @@ def move_down():
 
 
 while True:
-    print(test_print)
+    print('\n'.join(test_print))
     movements = input("up, down, left, right: ")
     if movements == "left":
         move_left()
