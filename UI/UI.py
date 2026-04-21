f12dec3629082f2c2a6a0be2ef6dc23d37a9de4d
James-Bergeron <202533444@cegepsherbrooke.qc.ca>
2026-04-21 12:22:59 -0400

    bouton settings in work

--- UI/UI.py
+++ UI/UI.py
@@ -1,3 +1,6 @@
+from turtle import pos
+from typing import Self
+
 import pygame 
 import sys
 
@@ -61,9 +64,79 @@ class Button:
             self.dynamic_elev = self.elevation
             self.top_color = "#038D05"
 
-# Création du bouton + loop d'affichage
+            
+#Création du bouton + loop d'affichage
 my_font = pygame.font.Font(None, 35)
 button = Button('Play!', 200, 40, (545, 400), 5)
+"""
+class Button_Settings:
+    def __init__(self, text, width, height, pos, elevation):
+        # Attributs de base
+        self.press = False
+        self.elevation = elevation
+        self.dynamic_elev = elevation
+        self.original_y_pos = pos[1]
+
+        # Rectangle du bouton
+        self.top_rect = pygame.Rect(pos, (width, height))
+        self.top_color = "#000000"
+
+        # Texte du bouton
+        self.text_surf = my_font.render(text, True, "#000202FF")
+        self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
+
+    def draw_settings(self):
+        # logique de l'élévation
+        self.top_rect.y = self.original_y_pos - self.dynamic_elev
+        self.text_rect.center = self.top_rect.center
+
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=12)
+        screen.blit(self.text_surf, self.text_rect)
+
+        self.check_click_settings()
+
+    def check_click_settings(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            self.top_color = "#000055"
+            if pygame.mouse.get_pressed()[0]:
+                self.dynamic_elev = 0
+                self.press = True
+            else:
+                self.dynamic_elev = self.elevation
+                if self.press == True:
+                    self.press = False
+                    # Indication du bouton, lorsqu'il est pressé
+                    print('Settings Pressed!')
+        else:
+            self.dynamic_elev = self.elevation
+            self.top_color = "#0F03B5"
+
+            
+
+# Création du bouton + loop d'affichage
+my_font = pygame.font.Font(None, 35)
+button = Button_Settings('Settings!', 300, 40, (545, 450), 5)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 running = True
 while running:
