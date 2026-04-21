01c951be75888939f40ff742b0b9409a7dfcc261
Luc Desforges <desforgesluc00@gmail.com>
2026-04-21 09:59:27 -0400

    fix props_interaction

--- Actors/player.py
+++ Actors/player.py
@@ -10,6 +10,8 @@ instanciated = False
 class Player(a.Actor):
 
     def __init__(self, 
+                 weapon: Weapon,
+                 potion_count: int,
                  level: int,
                  damage_done: int,
                  damage_received: int,
@@ -26,14 +28,22 @@ class Player(a.Actor):
         self.level = level
         self.damage_done = damage_done
         self.damage_received = damage_received
+        self.weapon = weapon
+        self.potion_count = potion_count
 
 
 def get_player():
     if(instanciated is False):
-        player_instance = Player(level = 1, 
+        player_instance = Player(weapon = None,
+                                 potion_count = 3,
+                                 level = 1, 
                                  damage_received = 0, 
                                  damage_done = 0,
                                  name = "Nard", 
                                  sprite = "@@",
                                  position = (0, 0))
     return player_instance
+
+class Weapon:
+    def __init__(self, name:str,):
+        self.name = name
