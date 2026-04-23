f14494b4ec6794ee30c0ecf3d3039dfa7eb35953
Luc Desforges <desforgesluc00@gmail.com>
2026-04-23 15:14:59 -0400

    moved old code to unclutter current code

--- Interactions/player_interactions.py
+++ Interactions/player_interactions.py
@@ -117,61 +117,6 @@ def insert_at_position(col: int, row: int, value: str):
     after_value = test_print[row][col*2+2:]
     test_print[row] = before_value + value + after_value
 
-"""
-def move_left():
-    global test_print
-    
-    pos = test_print.index(player)
-    if test_print[pos-2] == '#':
-        print("You can't move there")
-    else:
-        chars = list(test_print)
-        chars[pos] = air
-        chars[pos-2] = player
-        test_print = "".join(chars)
-    
-    
-def move_right():
-    global test_print
-    
-    pos = test_print.index(player)
-    if test_print[pos+2] == '##':
-        print("You can't move there")
-    else:
-        chars = list(test_print)
-        chars[pos] = air
-        chars[pos+2] = player
-        test_print = "".join(chars)
-def move_up():
-    global test_print
-    
-    test_print.splitlines()
-    
-    for lines in test_print.splitlines():
-        if player in lines:
-            pos = lines.index(player)
-            if test_print[test_print.index(lines)-len(lines)+pos] == walls:
-                print("You can't move there")
-            else:
-                chars = list(test_print)
-                chars[test_print.index(lines)+pos] = air
-                chars[test_print.index(lines)-len(lines)+pos-1] = player
-                test_print = "".join(chars)
-def move_down():
-    global test_print
-    test_print.splitlines()
-    for lines in test_print.splitlines():
-        if player in lines:
-            pos = lines.index(player)
-            if test_print[test_print.index(lines)+len(lines)+pos] == '#':
-                print("You can't move there")
-            else:
-                chars = list(test_print)
-                chars[test_print.index(lines)+pos] = air
-                chars[test_print.index(lines)+len(lines)+pos+1] = player
-                test_print = "".join(chars)
-"""
-
 
 while True:
     print('\n'.join(test_print))
