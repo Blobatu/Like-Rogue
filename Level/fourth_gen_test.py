18b5f6d8ffefd8c3d124f43af9003981ef233b88
Blobatu <antoinedesjardinsc@icloud.com>
2026-04-27 18:41:54 -0400

    wooo, fourth try's the charm right?

--- Level/fourth_gen_test.py
+++ Level/fourth_gen_test.py
@@ -1,3 +1,95 @@
+def make_lvl_data(maze):
+    lvl_data = []
+    for line in maze:
+        for i in range(1, 8):
+            lvl_data.append('')
+            for room in line:
+                if room == '':
+                    room = 'void'
+                lvl_data[-1] += ts[room][i]
+    return lvl_data
+
+def get_near(plan, x, y):
+    width = len(plan[0]) if plan else 0
+    height = len(plan)
+    neighbors = {'tl': None, 'tr': None, 'bl': None, 'br': None}
+    if x != width and y != height:
+        neighbors['tl'] = plan[y][x]
+        neighbors['tr'] = plan[y][x+1]
+        neighbors['bl'] = plan[y+1][x]
+        neighbors['br'] = plan[y+1][x+1]
+    return neighbors
+
+def gen_level():
+    plan = gen_template()
+    level = []
+    for y in range(len(plan)):
+        if y == len(plan):
+            continue
+        row = []
+        for x in range(len(plan[y])):
+            if x == len(plan[y]):
+                continue
+            w = get_near(plan, x, y)
+            # Assign tiles based on the combination of walls around
+            # r = right, l = left, u = up, d = down
+            # corners: tl, tr, bl, br
+            # If all directions open
+            if w['tl'] in ('r', 'd', 'l', 'u'):
+                # Cross (all open)
+                if w['tl'] == 'r' and w['tr'] == 'd' and w['bl'] == 'u' and w['br'] == 'l':
+                    row.append('hollow')
+                # Horizontal straight
+                elif w['tl'] == 'r' and w['tr'] == 'r' and w['bl'] == 'r' and w['br'] == 'r':
+                    row.append('strgt_horz')
+                # Vertical straight
+                elif w['tl'] == 'd' and w['tr'] == 'd' and w['bl'] == 'd' and w['br'] == 'd':
+                    row.append('strgt_vert')
+                # End right
+                elif w['tl'] == 'r' and w['tr'] != 'd' and w['bl'] == 'u' and w['br'] == 'l':
+                    row.append('end_r')
+                # End left
+                elif w['tl'] == 'l' and w['tr'] == 'd' and w['bl'] == 'u' and w['br'] != 'l':
+                    row.append('end_l')
+                # End bottom
+                elif w['tl'] == 'd' and w['tr'] == 'd' and w['bl'] != 'u' and w['br'] == 'l':
+                    row.append('end_b')
+                # End top
+                elif w['tl'] == 'u' and w['tr'] == 'd' and w['bl'] == 'u' and w['br'] != 'l':
+                    row.append('end_t')
+                # Corner left-bottom
+                elif w['tl'] == 'l' and w['tr'] == 'd' and w['bl'] != 'u' and w['br'] == 'l':
+                    row.append('crnr_lb')
+                # Corner left-top
+                elif w['tl'] == 'l' and w['tr'] == 'l' and w['bl'] == 'u' and w['br'] == 'l':
+                    row.append('crnr_lt')
+                # Corner right-bottom
+                elif w['tl'] == 'r' and w['tr'] == 'd' and w['bl'] == 'r' and w['br'] != 'l':
+                    row.append('crnr_rb')
+                # Corner right-top
+                elif w['tl'] == 'r' and w['tr'] == 'r' and w['bl'] == 'u' and w['br'] == 'r':
+                    row.append('crnr_rt')
+                # 3-way left-right-top
+                elif w['tl'] == 'l' and w['tr'] == 'l' and w['bl'] == 'u' and w['br'] == 'r':
+                    row.append('3way_lrt')
+                # 3-way left-right-bottom
+                elif w['tl'] == 'l' and w['tr'] == 'd' and w['bl'] == 'l' and w['br'] == 'l':
+                    row.append('3way_lrb')
+                # 3-way left-top-bottom
+                elif w['tl'] == 'l' and w['tr'] == 'l' and w['bl'] == 'd' and w['br'] == 'l':
+                    row.append('3way_ltb')
+                # 3-way right-top-bottom
+                elif w['tl'] == 'r' and w['tr'] == 'd' and w['bl'] == 'r' and w['br'] == 'd':
+                    row.append('3way_rtb')
+                # Cross
+                elif w['tl'] == 'd' and w['tr'] == 'r' and w['bl'] == 'l' and w['br'] == 'u':
+                    row.append('cross')
+                else:
+                    row.append('void')
+            else:
+                row.append('void')
+        level.append(row)
+    return level
 from cor_tile import tileset as ts
 from levels import levels as lev
 import random as rd
