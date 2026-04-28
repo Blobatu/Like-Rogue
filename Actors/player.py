4557062616582cec9cfbb1c14608708b605fb164
Luc Desforges <desforgesluc00@gmail.com>
2026-04-28 12:11:00 -0400

    bomb new propriety

--- Actors/player.py
+++ Actors/player.py
@@ -12,6 +12,7 @@ class Player(a.Actor):
     def __init__(self, 
                  weapon: Weapon,
                  potion_count: int,
+                 bomb_count: int,
                  level: int,
                  damage_done: int,
                  damage_received: int,
@@ -30,9 +31,12 @@ class Player(a.Actor):
         self.damage_received = damage_received
         self.weapon = weapon
         self.potion_count = potion_count
+        self.bomb_count = bomb_count
+
 
 player_instance: Player = Player(weapon=None,
                                  potion_count=3,
+                                 bomb_count=5,
                                  level=1, 
                                  damage_received=0, 
                                  damage_done=0,
