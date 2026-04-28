102be7e8fb2b5a93a7e035a23cfc4e250da72b10
Marvyn Mbeugmo <marvynmbeugmo@gmail.com>
2026-04-28 11:20:22 -0400

    Add files via upload

--- Level/levels.py
+++ Level/levels.py
@@ -1,3 +1,12 @@
+# Marvyn Mbeugmo
+# Ce module gère les différents niveaux du jeu en fonction de la progression du joueur.
+# Il définit les caractéristiques de chaque niveau, telles que la taille, la difficulté et si le niveau est personnalisé ou non.
+
+import sys
+sys.path.append(r"c:\Users\Elitebook\OneDrive - Cegep de Sherbrooke\Documents\Sessiion 2\Programmation\Exercise et Projet\Projet 3")
+
+from Progression import actor
+
 levels = {
     'level_1' : {
         'size' : (16, 7),
@@ -9,14 +18,11 @@ levels = {
         'difficulty' : 3,
         'custom' : False,
     },
-    'level_3' : {
-        'size' : (16, 7),
-        'difficulty' : 5,
-        'custom' : False,
-    },
-    'level_4' : {
-        'size' : (16, 7),
-        'difficulty' : 7,
-        'custom' : False,
-    },
 }
+
+def get_current_level(player):
+    """Retourne le niveau en fonction du level du joueur"""
+    if player.level == 1:
+        return levels['level_1']
+    else:
+        return levels['level_2']
