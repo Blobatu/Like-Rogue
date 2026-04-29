6732b636adfcea5910046e1c8437a7a36e0f16cf
Blobatu <antoinedesjardinsc@icloud.com>
2026-04-29 09:51:14 -0400

    modifs

--- Level/level_generation.py
+++ Level/level_generation.py
@@ -210,7 +210,7 @@ def full_level(maze: list[list[str]] = make_level(),
         elif (y, tile_x) in all_selected[enn_count + chest_count:]:
             new_data[y][tile_x:tile_x+2] = ['△', ' ']
         else:
-            new_data[y][tile_x:tile_x+2] = ['&', '&']
+            new_data[y][tile_x:tile_x+2] = ['.', '&']
     
     return [''.join(line) for line in new_data]
 
