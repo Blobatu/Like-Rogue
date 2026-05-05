e501136f9e69b6377f8e847e8f8f6a3a12dfc9f0
Luc Desforges <desforgesluc00@gmail.com>
2026-05-05 15:21:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,13 @@ monster_spawn.spawn_monster()
 
 while(True):
     if p_i.is_alive():
-        #print("\033[H\033[J", end="")
+        print("\033[H\033[J", end="")
         print("".join(columns_legend()))
         print(p_i.position)
         print(p_i.level)
+        print(str(p_i.health)+"/"+str(p_i.max_health)+"HP")
+        print(str(p_i.bomb_count)+" bomb(s)")
+        print(str(p_i.potion_count)+" potion(s)")
         print('\n'.join(dungeon.levels[p_i.level-1]))
         p_m.listen_to_keyboard()
         monster_AI.react_to_player_movement()
